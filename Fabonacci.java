03e10a6089da469bc4d5683c4abf433ebff834b9
Utkarsh Sharma <utkarsh.sharma_cs22@gla.ac.in>
2023-08-25 21:56:01 +0530

    "1"

--- Fabonacci.java
+++ Fabonacci.java
@@ -1,16 +1,20 @@
 import java.util.Scanner;
-public class Fabonacci{
+public class Fabonacci {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter-");
         int n=sc.nextInt();sc.close();
-        System.out.printf("0\n1");
-        int a=0,b=1,c;
-        for(int i=2;i<n;++i){
-            c=a+b;
-            System.out.println(c);
-            a=b;
-            b=c;
+        if (n==1)System.out.println("0");
+        else if (n==2) System.out.println("0\n1");
+        else{
+            System.out.printf("0\n1\n");
+            int a=0,b=1,c;
+            for(int i=2;i<n;++i){
+                c=a+b;
+                System.out.println(c);
+                a=b;
+                b=c;
+            }
         }
     }
 }
